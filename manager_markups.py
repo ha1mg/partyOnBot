c60c54d4bb8373acdb7b5ccfeb4bfb01df5adb88
Vadim Ya <iakovlev.vadim@list.ru>
2023-07-18 03:09:31 +0300

    Hide future feature

--- manager_markups.py
+++ manager_markups.py
@@ -1,8 +1,8 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 
-btnList = KeyboardButton('Список постов')
+# btnList = KeyboardButton('Список постов')
 btnNew = KeyboardButton('Новый пост')
-mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnList, btnNew)
+mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnNew)
 
 btnSave = InlineKeyboardButton('Сохранить', callback_data='save')
 btnEdit = InlineKeyboardButton('Изменить', callback_data='edit')
